5a5c061471a6f63a1f083e74925b6ae2d3ea180a
Ian <iankruze60@gmail.com>
2025-05-23 18:35:00 +0300

    Practise to SELECT, INSERT, and CREATE TABLE in sql

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 25, 'M', 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 8, 'F', 'Gray', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 12, 'M', 'Dark Brown', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 6, 'F', 'White', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 4, 'M', 'Green', 'Grumpy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 7, 'F', 'Brown', 'Sweet', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, 'M', 'Black', 'Shy', 1);
